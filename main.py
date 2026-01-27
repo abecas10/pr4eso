eaa6a335019f7902f4858c896fd2739579287312
albecas10 <warmesg@gmail.com>
2026-01-27 09:42:10 +0100

    27/01/26 9:42

--- main.py
+++ main.py
@@ -1,127 +1,85 @@
+'''
+This is a simple program that allows you to buy products and find similar products based on tags.
+
+Tags are used to categorize products. For example, a product tagged as "fruit" and "healthy" is a fruit that is healthy.
+
+The program allows you to buy a product and find similar products based on tags.
+
+It is a simple implementation of a recommendation system.
+
+This program is made by: Alex Castella & Gabriel Cereto
+'''
 
 
-class product:
-    def __init__(self,name,price,quantity, tags):
-        self.name = name
-        self.price = price
-        self.quantity = quantity
-        self.tags = tags
+products = {
+    "Apple": ["fruit", "food", "healthy", "grocery"],
+    "Banana": ["fruit", "food", "healthy", "grocery"],
+    "Orange": ["fruit", "food", "healthy", "grocery"],
+    "Computer": ["electronics", "technology", "pc"],
+    "Laptop": ["electronics", "technology", "pc"],
+    "Smartphone": ["electronics", "technology", "phone"],
+    "Smartwatch": ["electronics", "technology", "watch", "phone"],
+    "Weight": ["healthy", "fitness", "gym", "sports"],
+    "BotleWater": ["healthy", "fitness", "gym", "sports", "drink"],
+    "Coffee": ["food", "grocery", "coffee", "drink"],
+    "Milk": ["food", "grocery", "coffee", "drink", "healthy"],
+    "Tea": ["food", "grocery", "coffee"],
+    "Cookies": ["food", "grocery", "coffee", "snack", "sweet"],
+    "Chocolate": ["food", "grocery", "snack", "sweet"],
+    "Candy": ["food", "grocery", "snack", "sweet"],
+    "Camera": ["electronics", "technology", "camera"],
+    "Headphones": ["electronics", "technology", "phone", "watch"],
+    "PhoneCase": ["electronics", "technology", "phone"],
+    "ScreenProtector": ["electronics", "technology", "phone"],
+    "Mouse": ["electronics", "technology", "pc"],
+    "Keyboard": ["electronics", "technology", "pc"],
+    "Monitor": ["electronics", "technology", "pc", "gaming"],
+    "Printer": ["electronics", "technology", "pc"],
+    "Console": ["electronics", "technology", "gaming"],
+    "GameController": ["electronics", "technology", "gaming"],
+    "Tv": ["electronics", "technology", "gaming", "tv"],
+    "Batteries": ["electronics", "technology", "tv"],
+    "BasketBall": ["sports", "fitness", "basketball"],
+    "Football": ["sports", "fitness", "football"],
+    "EnergyDrink": ["sports", "fitness", "gym", "drink"],
+    "Baseball": ["sports", "fitness", "baseball"],
+    "TennisRacket": ["sports", "fitness", "tennis"],
+    "Tennisball": ["sports", "fitness", "tennis"],
+    "Basketball_Shoes": ["sports", "fitness", "basketball"],
+    "Basketball_Shirt": ["sports", "fitness", "basketball"],
+    "Football_Shoes": ["sports", "fitness", "football"],
+    "Football_Shirt": ["sports", "fitness", "football"],
+    "Energybar": ["sports", "fitness", "gym"],
+    "Baseball_bat": ["sports", "fitness", "baseball"],
+    "Baseball_cap": ["sports", "fitness", "baseball"],
+    "Baseball_glove": ["sports", "fitness", "baseball"],
+    "Baseball_shirt": ["sports", "fitness", "baseball"],
+    "Dog_Food": ["pet", "dog", "food", "grocery"],
+    "Dog_champoo": ["pet", "dog", "grocery"],
+    "Dog_toy": ["pet", "dog", "toy", "grocery"],
+    "Cat_Food": ["pet", "cat", "food", "grocery"],
+    "Cat_toy": ["pet", "cat", "toy", "grocery"],
+    "Beer": ["food", "grocery", "drink", "alcohol"],
+    "Wine": ["food", "grocery", "drink", "alcohol"],
+    "Popcorn": ["food", "grocery", "snack", "salty", "tv"],
+    "Soda": ["food", "grocery", "drink", "tv"],
+    "Juice": ["food", "grocery", "drink", "healthy"],
+    "Eggs": ["food", "grocery", "healthy"],
+    "Tomato": ["food", "grocery", "healthy"]
+}
 
-    def display_info(self):
-        print(f"Product Name: {self.name}")
-        print(f"Price: ${self.price}")
-        print(f"Quantity: {self.quantity}")
-        print(f"Tags: {', '.join(self.tags)}")
+print(len(products))
 
+buy = input("Buy a product: ")
 
-print("-------------------------")
+print("--------------")
 
-Apple = product("Apple", 0.99, 100, ["fruit", "food", "healthy", "grocery"])
-Apple.display_info()
+if buy in products:
+    print(f"Tags: {products[buy]}")
+else:
+    print("Error: Product not found")
 
-print("-------------------------")
+print("--------------")
+print("Finding similar products...")
 
-Banana = product("Banana", 0.99, 100, ["fruit", "food", "healthy", "grocery"])
-Banana.display_info()
-
-print("-------------------------")
-
-Orange = product("Orange", 0.99, 100, ["fruit", "food", "healthy", "grocery"])
-Orange.display_info()
-
-print("-------------------------")
-
-Computer = product("Computer", 999.99, 50, ["electronics", "technology", "pc"])
-Computer.display_info()
-
-print("-------------------------")
-
-Laptop = product("Laptop", 999.99, 50, ["electronics", "technology", "pc"])
-Laptop.display_info()
-
-print("-------------------------")
-
-Smartphone = product("Smartphone", 999.99, 50, ["electronics", "technology", "phone"])
-Smartphone.display_info()
-
-print("-------------------------")
-
-SmartWatch = product("Smartwatch", 999.99, 50, ["electronics", "technology", "watch", "phone"])
-SmartWatch.display_info()
-
-print("-------------------------")
-
-Weights = product("Weight", 0.99, 100, ["healthy", "fitness", "gym", "sports"])
-Weights.display_info()
-
-print("-------------------------")
-
-BotleWater = product("BotleWater", 0.99, 100, ["healthy", "fitness", "gym", "sports"])
-BotleWater.display_info()
-
-print("-------------------------")
-
-Coffee = product("Coffee", 0.99, 100, ["food", "grocery", "coffee"])
-Coffee.display_info()
-
-print("-------------------------")
-
-Milk = product("Milk", 0.99, 100, ["food", "grocery", "coffee"])
-Milk.display_info()
-
-print("-------------------------")
-
-Tea = product("Tea", 0.99, 100, ["food", "grocery", "coffee"])
-Tea.display_info()
-
-print("-------------------------")
-
-Cookies = product("Cookies", 0.99, 100, ["food", "grocery", "coffee", "snack"])
-Cookies.display_info()
-
-print("-------------------------")
-
-Chocolate = product("Chocolate", 0.99, 100, ["food", "grocery", "snack"])
-Chocolate.display_info()
-
-print("-------------------------")
-
-Candy = product("Candy", 0.99, 100, ["food", "grocery", "snack"])
-Candy.display_info()
-
-print("-------------------------")
-
-Camera = product("Camera", 999.99, 50, ["electronics", "technology", "camera"])
-Camera.display_info()
-
-print("-------------------------")
-
-Headphones = product("Headphones", 999.99, 50, ["electronics", "technology", "phone", "watch"])
-Headphones.display_info()
-
-print("-------------------------")
-
-PhoneCase = product("PhoneCase", 999.99, 50, ["electronics", "technology", "phone"])
-PhoneCase.display_info()
-
-print("-------------------------")
-
-ScreenProtector = product("ScreenProtector", 999.99, 50, ["electronics", "technology", "phone"])
-ScreenProtector.display_info()
-
-print("-------------------------")
-
-Mouse = product("Mouse", 999.99, 50, ["electronics", "technology", "pc"])
-Mouse.display_info()
-
-print("-------------------------")
-
-Keyboard = product("Keyboard", 999.99, 50, ["electronics", "technology", "pc"])
-Keyboard.display_info()
-
-print("-------------------------")
-
-Monitor = product("Monitor", 999.99, 50, ["electronics", "technology", "pc"])
-Monitor.display_info()
-
-print("-------------------------")
+similar_products = []
